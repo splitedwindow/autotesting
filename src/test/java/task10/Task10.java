eac79d0b5329d9d7d975418f5bf050bb08fcc89b
Roman <rom@kozova.net>
2024-10-12 18:52:29 +0300

    it was task 10, just renamed

--- src/test/java/task10/Task10.java
+++ src/test/java/task10/Task10.java
@@ -1,19 +1,16 @@
-package task11;
+package task10;
 
 import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
-public class Task11 {
+public class Task10 {
     WebDriver driver;
     public static void main(String[] args) {
 
